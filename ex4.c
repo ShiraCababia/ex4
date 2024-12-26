d4684cf0380211cf978630e7b0fd446bac352c03
Shira Cababia <shiraca200@gmail.com>
2024-12-26 02:32:14 +0200

    Case 2 - The Human Pyramid

--- ex4.c
+++ ex4.c
@@ -6,10 +6,15 @@ Assignment: EX4
 #include <stdio.h>
 #include <string.h>
 
-void task1RobotPaths();
-int robotPath(int curUp, int curRight); //1 
-void task2HumanPyramid();
-void task3ParenthesisValidator();
+#define LEVEL_OF_PYRAMID 5
+// #define NUM_OF_CHEERLEADERS 15
+
+void task1RobotPaths(); // 1
+int robotPath(int upCoordinate, int rightCoordinate);
+void task2HumanPyramid(); // 2
+void initializeChrldrsArr(float cheerleadersWeights[LEVEL_OF_PYRAMID][LEVEL_OF_PYRAMID]);
+float weightsPyramid(int row, int cloumn, float cheerleadersWeights[LEVEL_OF_PYRAMID][LEVEL_OF_PYRAMID]);
+void task3ParenthesisValidator(); // 3
 void task4QueensBattle();
 void task5CrosswordGenerator();
 
@@ -30,13 +35,13 @@ int main()
         {
             switch (task)
             {
-            case 6:
+            case 6: // Done
                 printf("Goodbye!\n");
                 break;
-            case 1:
+            case 1: // Done
                 task1RobotPaths();
                 break;
-            case 2:
+            case 2: // Done
                 task2HumanPyramid();
                 break;
             case 3:
@@ -48,7 +53,7 @@ int main()
             case 5:
                 task5CrosswordGenerator();
                 break;
-            default:
+            default: // Done
                 printf("Please choose a task number from the list.\n");
                 break;
             }
@@ -61,7 +66,8 @@ int main()
     } while (task != 6);
 }
 
-// Case 1
+// Case 1 :
+// The func gets the coordinates from the user, use the recursive function "robotPath" and print the number of paths.
 void task1RobotPaths()
 {
     int upCoordinate, rightCoordinate, numPaths;
@@ -71,24 +77,85 @@ void task1RobotPaths()
     printf("The total number of paths the robot can take to reach home is: %d\n", numPaths);
 }
 
-int robotPath(int curUp, int curRight)
+/* The func goes through all the possible options of movements from the given coordinate to (0,0) and eventually
+returns the number of possible paths to reach it. */
+int robotPath(int upCoordinate, int rightCoordinate)
 {
-    if (curUp == 0 && curRight == 0)
+    if (upCoordinate == 0 && rightCoordinate == 0)
     {
         return 1;
     }
-    else if (curUp < 0 || curRight < 0)
+    else if (upCoordinate < 0 || rightCoordinate < 0)
     {
         return 0;
     }
-    return (robotPath((curUp - 1), curRight) + robotPath(curUp, (curRight - 1)));
+    return (robotPath((upCoordinate - 1), rightCoordinate) + robotPath(upCoordinate, (rightCoordinate - 1)));
 }
 
+// Case 2 :
+/* The fun initialize the cheerleaders' weights array to invalid weight (-1), gets the weights from the user, 
+use the recursive function "weightsPyramid" and print the total weight for every cheerleader.*/
 void task2HumanPyramid()
 {
-    // Todo
+    float cheerleadersWeights[LEVEL_OF_PYRAMID][LEVEL_OF_PYRAMID];
+    initializeChrldrsArr(cheerleadersWeights);
+    printf("Please enter the weights of the cheerleaders:\n");
+    for (int i = 0; i < LEVEL_OF_PYRAMID; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            float weight;
+            scanf(" %f", &weight);
+            if (weight < 0)
+            {
+                printf("Negative weights are not supported.\n");
+                return;
+            }
+            cheerleadersWeights[i][j] = weight;
+        }
+    }
+    printf("The total weight on each cheerleader is:\n");
+    for (int i = 0; i < LEVEL_OF_PYRAMID; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            float totalWeight = weightsPyramid(i, j, cheerleadersWeights);
+            printf("%.2f ", totalWeight);
+        }
+        printf("\n");
+    }
 }
 
+// The func initialize the values in the array contains the cheerleaders' weights.
+void initializeChrldrsArr(float cheerleadersWeights[LEVEL_OF_PYRAMID][LEVEL_OF_PYRAMID])
+{
+    for (int i = 0; i < LEVEL_OF_PYRAMID; i++)
+    {
+        for (int j = 0; j < LEVEL_OF_PYRAMID; j++)
+        {
+            cheerleadersWeights[i][j] = -1;
+        }
+    }
+}
+
+/* The func sum the total weight supported by the cheerleader in the given row and column.*/
+float weightsPyramid(int row, int cloumn, float cheerleadersWeights[LEVEL_OF_PYRAMID][LEVEL_OF_PYRAMID])
+{
+    if (row == 0 && cloumn == 0)
+    {
+        return cheerleadersWeights[row][cloumn];
+    }
+    // If there is no cheerleader in the top left / right.
+    else if (row < 0 || cloumn < 0 || cheerleadersWeights[row][cloumn] < 0)
+    {
+        return 0;
+    }
+    float ownWeight = cheerleadersWeights[row][cloumn];
+    return ((0.5 * weightsPyramid((row - 1), (cloumn - 1), cheerleadersWeights)) +
+            (0.5 * weightsPyramid((row - 1), cloumn, cheerleadersWeights)) + ownWeight);
+}
+
+// Case 3
 void task3ParenthesisValidator()
 {
     // Todo
